e4bc0180ff8e3e94fbc3f7159870249a5cd9dd3b
Jørgen Galdal <jorggal@stud.ntnu.no>
2022-09-30 22:00:17 +0200

    Corrected out-of-sync bouncing

--- main.py
+++ main.py
@@ -2,34 +2,37 @@ from PIL import Image
 from PIL import ImageColor
 from PIL import ImageDraw
 
-FILE_BASE = "60BPM_"
-FILE_EXT = "png"
-
-duration = 15 # Duration of animation (in seconds)
-fps = 20 # frames per second
-frames = fps*duration
 
 BPM = 60
-size_multi = 120
-velocity = (size_multi*(BPM/60))/fps
+duration = 30  # Duration of animation (in seconds)
+fps = 20  # frames per second
+frames = fps*duration
 
-point_size = 10
+FILE_BASE = f"{BPM}BPM_"
+FILE_EXT = "png"
+
 border = 10
+resolution = 20  # Size of ball
+multiplier = 4  # How many balls will fit inside one meter
+
+velocity = (resolution*multiplier*(BPM/60))/fps
+
 
 direction = [1, 1]
 
 def create_base(meter_a, meter_b):
-    """Creates a visual a polyrhythm of meter_a against meter_b.
+    """Creates a visual of a polyrhythm of meter_a against meter_b.
     """
-    image_size = (meter_a*size_multi, meter_b*size_multi)
+    image_size = (2*border+resolution*(meter_a*multiplier+1), 
+                  2*border+resolution*(meter_b*multiplier+1))
     
     img = Image.new("RGB", image_size, "#FFFFFF")
 
     canvas = ImageDraw.Draw(img)
     
     canvas.rectangle([(0,0), image_size],
-                 outline="#000000",
-                 width=border)
+                     outline="#000000",
+                     width=border)
 
     return img
 
@@ -37,13 +40,10 @@ def create_base(meter_a, meter_b):
 def create_frame(base_img, pos):
     new = base_img.copy()
     canvas = ImageDraw.Draw(new)
-    br = [i+point_size for i in pos]
-    canvas.rectangle(pos + br, fill="#000000")
+    bottom_right = [i+resolution for i in pos]  # Bottom right pos of point rectangle
+    canvas.rectangle(pos + bottom_right, fill="#FF0000")
     return new
     
-    
-
-
 
 if __name__ == "__main__":
     meter_a = int(input("Meter A: "))
@@ -52,16 +52,18 @@ if __name__ == "__main__":
     img = create_base(meter_a, meter_b)
 
     pos = [border, border]
+
     for frame in range(frames):
         file_name = f"frames\{FILE_BASE}{frame}.{FILE_EXT}"
         create_frame(img, pos).save(file_name)
         print(f"Created frame {frame} of {frames} ({file_name})")
-        pos[0] += velocity*direction[0]
-        pos[1] += velocity*direction[1]
-        if pos[0] =< border or pos[0] + point_size >= (img.width - border):
-            direction[0] = -direction[0]
-        if pos[1] =< border or pos[1] + point_size >= (img.height - border):
-            direction[1] = -direction[1]
+
+        for i in range(2):
+            pos[i] += velocity*direction[i]
+
+            # Checks if point hits border
+            if pos[i] <= border or pos[i] + resolution >= (img.size[i] - border):
+                direction[i] = -direction[i]
 
 
 
