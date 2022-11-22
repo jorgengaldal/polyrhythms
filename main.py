e7d1ab78a18c73b6cb13ac8a0e0b2f76e724cdc9
Jørgen Galdal <jorggal@stud.ntnu.no>
2022-11-22 15:19:24 +0100

    Initial OOP-rewrite

--- main.py
+++ main.py
@@ -12,176 +12,183 @@ import cv2 as cv
 import os
 
 
-def create_base(meter_a, meter_b,
-                border=10,
-                resolution=20,
-                multiplier=4):
-    """Creates a visual of a polyrhythm of meter_a against meter_b.
-    args:
-    meter_a: horisontal meter
-    meter_b: vertical meter
-    resolution: size of ball
-    border: border size in pixels
-    multiplier: How many balls will fit inside one division (*meter* number of divisions in a beat)
-    """
-    image_size = (2*border+resolution*(meter_a*multiplier+1),
-                  2*border+resolution*(meter_b*multiplier+1))
-
-    img = Image.new("RGB", image_size, "#FFFFFF")
-
-    canvas = ImageDraw.Draw(img)
-
-    canvas.rectangle([(0, 0), image_size],
-                     outline="#000000",
-                     width=border)
-
-    return img
-
-
-def create_frame(base_img,
-                 pos,
-                 resolution=20):
-    """Creates single frame
-    args:
-    base_img: image to make frame from
-    pos: current posision of point
-    resolution: size of point
-    """
-    new = base_img.copy()
-    canvas = ImageDraw.Draw(new)
-    # Bottom right pos of point rectangle
-    bottom_right = [i+resolution for i in pos]
-    canvas.rectangle(pos + bottom_right, fill="#FF0000")
-    return new
-
-
-def create_frames(base_img,
-                  duration=30,
-                  fps=30,
-                  bpm=60,
-                  resolution=20,
-                  border=10,
-                  multiplier=4,
-                  frame_dir="frames",
-                  file_extension="png"):
-    """TODO: Complete docstring
-    args:
-    base_img: image to create frames from
-    duration: number of seconds the video will last
-    fps: frames per second
-    bpm: beats per minute
-    resolution: size of ball
-    border: border size in pixels
-    multiplier: How many balls will fit inside one division (*meter* number of divisions in a beat)
-    frame_dir: directory to save frames to
-    file_extension: file extension for frames
-    """
-
-    file_base = f"{bpm}BPM_"
-
-    frames = duration * fps
-
-    reference_beat = min(base_img.size)
-    velocity_per_second = (reference_beat) / (bpm / 60) 
-    velocity = int(velocity_per_second / fps) 
-    print(velocity)
-
-    pos = [border, border]
-    direction = [1, 1]
-
-    for frame in range(frames):
-        file_name = f"{frame_dir}\{file_base}{frame}.{file_extension}"
-        create_frame(base_img, pos, resolution).save(file_name)
-        print(f"Created frame {frame} of {frames} ({file_name})")
-
-        move_point(base_img, pos, direction, velocity, border, resolution)
-
-
-def move_point(base_img,
-              pos,
-              direction,
-              velocity,
-              border,
-              resolution):
-    """
-    Does in-place modification of pos.
-    args:
-    base_img: image from which the size is to be calculated
-    pos: list of point's position
-    direction: list of the point's current moving direction
-    velocity: Number of pixels to move
-    border: border size in pixels
-    resolution: size of ball
-    """
-    
-    for i in range(2):
-        wanted_move = velocity * direction[i]
-
-        direction[i] *= -1  # Assumes border hit
-
-        # Checks first (left/top) border hit
-        if (pos[i] + wanted_move) <= border:
-            print("First border hit")
-            to_border_move = pos[i] - border
-            from_border_move = (-wanted_move) - to_border_move
-            final_move = -(to_border_move - from_border_move)
-            print(f"{pos=}, {to_border_move=}, {wanted_move=}")
-        
-        # Checks last (right/bottom) border hit
-        elif (pos[i] + wanted_move + resolution) >= (base_img.size[i] - border):
-            print("Last border hit")
-            to_border_move = (base_img.size[i] - border) - (pos[i] + resolution)
-            from_border_move = wanted_move - to_border_move
-            final_move = to_border_move - from_border_move
-
-        # No border hit
-        else:
-            final_move = wanted_move
-            direction[i] *= -1  # Reverts border hit assumption
-
-        pos[i] += final_move
-
-
-def create_video(img_dir="frames",
+class PolyRhythmVisual:
+    def __init__(self,
+                 meter_a,
+                 meter_b,
+                 border = 10,
+                 resolution = 20,
+                 multiplier = 1,
+                 duration = 15,
+                 fps = 30,
+                 bpm = 60,
+                 frames_directory="frames",
                  destination="output.avi",
-                 fps=30,
-                 fourcc="DIVX"):
-    """ Creates a video with OpenCV and returns path to video
-    args:
-    img_dir - path to directory of frames
-    destination - output file destination
-    fps - frames per second
-    fourcc - string representation of FourCC video codec
-    """
+                 img_extension="png",
+                 codec="DIVX"):
+        """
+        TODO Add docstring
+        Creates a visual of a polyrhythm of meter_a against meter_b.
 
-    images = []
+        args:
+        meter_a: horisontal meter
+        meter_b: vertical meter
+        resolution: size of ball
+        border: border size in pixels
+        multiplier: How many balls will fit inside one division (*meter* number of divisions in a beat)
+        duration: number of seconds the video will last
+        fps: frames per second
+        bpm: beats per minute
+        frames_directory: directory to save frames to
+        img_extension: file extension for frames
+        codec - string representation of FourCC video codec
+        """
 
-    fourcc_object = cv.VideoWriter_fourcc(*fourcc)
-    for filename in sorted(glob.glob(f"{img_dir}/*"), key=os.path.getmtime):
-        img = cv.imread(filename)
-        images.append(img)
+        self.meter_a = meter_a
+        self.meter_b = meter_b
+        self.border = border
+        self.resolution = resolution
+        self.multiplier = multiplier
+        self.duration = duration
+        self.fps = fps
+        self.bpm = bpm
+        self.frames_directory = frames_directory
+        self.destination = destination
+        self.img_extension = img_extension
+        self.codec = codec
+        
+        self.size = self._calculate_size()
+        self.velocity = self._calculate_velocity()
+        self.pos = [self.border, self.border]
+        self.direction = [1, 1]
+        self.frames = self.duration * self.fps
 
-    height, width, _ = images[0].shape
-    size = (width, height)
+        self.base = self._create_base()
 
-    output = cv.VideoWriter(destination, fourcc_object, fps, size)
 
-    for im in images:
-        output.write(im)
+    @classmethod
+    def one_loop(self):
+        # TODO
+        # Makes an object with the duration of one loop
+        pass
 
-    output.release()
+    def _calculate_size(self):
+        # TODO Add documentation
+        return (2*self.border+self.resolution*(self.meter_a*self.multiplier+1),
+                2*self.border+self.resolution*(self.meter_b*self.multiplier+1))
 
-    print(f"Video written to {destination}")
+    def _calculate_velocity(self):
+        # TODO Add documentation
+        reference_beat = min(self.size)
+        velocity_per_second = (reference_beat) / (self.bpm / 60)
+        velocity = int(velocity_per_second / self.fps)
+        return velocity
 
-    return destination
+    def _create_base(self):
+        """
+        TODO Add docstring
+        """
+
+        img = Image.new("RGB", self.size, "#FFFFFF")
+
+        canvas = ImageDraw.Draw(img)
+
+        canvas.rectangle([(0, 0), self.size],
+                        outline="#000000",
+                        width=self.border)
+
+        return img
+
+    def _create_frame(self):
+        """Creates single frame"""
+
+        new = self.base.copy()
+        canvas = ImageDraw.Draw(new)
+
+        # Bottom right pos of point rectangle
+        bottom_right = [i+self.resolution for i in self.pos]
+
+        canvas.rectangle(self.pos + bottom_right, fill="#FF0000")
+        return new
+
+    def _create_frames(self):
+        """TODO Complete docstring
+        args:
+        """
+
+        file_base = f"{self.bpm}BPM_"
+
+        for frame in range(self.frames):
+            file_name = f"{self.frames_directory}\{file_base}{frame}.{self.img_extension}"
+            self._create_frame().save(file_name)
+            print(f"Created frame {frame} of {self.frames} ({file_name})")
+
+            self._move_point()
+
+    def _move_point(self):
+        """
+        Does in-place modification of pos.
+        """
+
+        for i in range(2):
+            wanted_move = self.velocity * self.direction[i]
+
+            self.direction[i] *= -1  # Assumes border hit
+
+            # Checks first (left/top) border hit
+            if (self.pos[i] + wanted_move) <= self.border:
+                # print("First border hit")
+                to_border_move = self.pos[i] - self.border
+                from_border_move = (-wanted_move) - to_border_move
+                final_move = -(to_border_move - from_border_move)
+                # print(f"{self.pos=}, {to_border_move=}, {wanted_move=}")
+
+            # Checks last (right/bottom) border hit
+            elif (self.pos[i] + wanted_move + self.resolution) >= (self.size[i] - self.border):
+                # print("Last border hit")
+                to_border_move = (
+                    self.size[i] - self.border) - (self.pos[i] + self.resolution)
+                from_border_move = wanted_move - to_border_move
+                final_move = to_border_move - from_border_move
+
+            # No border hit
+            else:
+                final_move = wanted_move
+                self.direction[i] *= -1  # Reverts border hit assumption
+
+            self.pos[i] += final_move
+
+    def _create_video(self):
+        """
+        Creates a video with OpenCV and returns path to video
+        """
+        # TODO Clear frames_directory
+
+        images = []
+
+        fourcc_object = cv.VideoWriter_fourcc(*self.codec)
+        for filename in sorted(glob.glob(f"{self.frames_directory}/*"), key=os.path.getmtime):
+            img = cv.imread(filename)
+            images.append(img)
+
+        output = cv.VideoWriter(self.destination, fourcc_object, self.fps, self.size)
+
+        for im in images:
+            output.write(im)
+
+        output.release()
+
+        print(f"Video written to {self.destination}")
+
+        # return destination
 
 
 if __name__ == "__main__":
     meter_a = int(input("Meter A: "))
     meter_b = int(input("Meter B: "))
 
-    img = create_base(meter_a, meter_b)
+    visual = PolyRhythmVisual(meter_a, meter_b)
 
-    create_frames(img)
+    visual._create_frames()
 
-    create_video()
+    visual._create_video()
